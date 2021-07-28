a50ab567d76d7b9410ddc256c5f9a054bcf09e5d
Nikolay Pultsin <geometer@fbreader.org>
2021-07-28 02:58:26 +0100

    formatting

--- math/diagram/diagram_cross.cpp
+++ math/diagram/diagram_cross.cpp
@@ -1,42 +1,40 @@
-#include <stdlib.h>
-
 #include "diagram.h"
 
-bool diagram::tryAddCrossing (vertex *v1, vertex *v2)
-{
-  if (!crossed (v1, v2))
-    return 0;
+bool diagram::tryAddCrossing(vertex *v1, vertex *v2) {
+	if (!crossed(v1, v2)) {
+		return false;
+	}
 
-  new crossing (v1, v2);
+	new crossing (v1, v2);
 
-  return 1;
+	return true;
 }
 
-bool diagram::tryChangeCrossing (vertex *v1, vertex *v2)
-{
-  if (tryRemoveCrossing (v2, v1))
-    return tryAddCrossing (v1, v2);
+bool diagram::tryChangeCrossing(vertex *v1, vertex *v2) {
+	if (tryRemoveCrossing (v2, v1)) {
+		return tryAddCrossing (v1, v2);
+	}
 
-  return 0;
+	return false;
 }
 
-bool diagram::tryRemoveCrossing (vertex *v1, vertex *v2)
-{
-  for (crossing *c = v1 -> crs (); c; c = c -> next ())
-    if (c -> up () == v2)
-    {
-      delete c;
-      return 1;
-    }
+bool diagram::tryRemoveCrossing(vertex *v1, vertex *v2) {
+	for (crossing *c = v1->crs(); c; c = c->next()) {
+		if (c->up() == v2) {
+			delete c;
+			return true;
+		}
+	}
 
-  return 0;
+	return false;
 }
 
-bool diagram::isCrossing (vertex *v1, vertex *v2)
-{
-  for (crossing *c = v1 -> crs (); c; c = c -> next ())
-    if (c -> up () == v2)
-      return 1;
+bool diagram::isCrossing(vertex *v1, vertex *v2) {
+	for (crossing *c = v1->crs(); c; c = c->next()) {
+		if (c->up() == v2) {
+			return true;
+		}
+	}
 
-  return 0;
+	return false;
 }

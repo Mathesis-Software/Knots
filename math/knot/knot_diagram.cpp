d439393e204ead6e470b04c6668351d66f7e4e0a
Nikolay Pultsin <geometer@fbreader.org>
2021-07-29 18:18:46 +0100

    formatting

--- math/knot/knot_diagram.cpp
+++ math/knot/knot_diagram.cpp
@@ -6,37 +6,37 @@
 namespace KE { namespace ThreeD {
 
 Knot::Knot(diagram *D, int w, int h) : caption("Converted from diagram") {
-  vertex *v = D -> base;
+  vertex *v = D->base;
 	do {
 		points.push_back(Point(
-			2.4 * v -> x () / w - 1.2,
-			1.2 - 2.4 * v -> y () / h,
+			2.4 * v->x() / w - 1.2,
+			1.2 - 2.4 * v->y() / h,
 			0
 		));
 		
-		crossing *crs = v -> crs ();
+		crossing *crs = v->crs();
 		while (crs) {
 			points.push_back(Point(
-				2.4 * crs -> x () / w - 1.2,
-				1.2 - 2.4 * crs -> y () / h,
+				2.4 * crs->x() / w - 1.2,
+				1.2 - 2.4 * crs->y() / h,
 				-0.4
 			));
-			if (crs -> next ()) {
+			if (crs->next()) {
 				points.push_back(Point(
-					1.2 * (crs -> x () + crs -> next () -> x ()) / w - 1.2,
-					1.2 - 1.2 * (crs -> y () + crs -> next () -> y () ) / h,
+					1.2 * (crs->x() + crs->next()->x()) / w - 1.2,
+					1.2 - 1.2 * (crs->y() + crs->next()->y()) / h,
 					0
 				));
 			}
-			crs = crs -> next ();
+			crs = crs->next();
 		}
-		v = v -> next ();
-	} while (v != D -> base);
+		v = v->next();
+	} while (v != D->base);
 
   create_depend();
 
   normalize(4 * this->points.size());
-  center ();
+  center();
 }
 
 }}

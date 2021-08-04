4e95923b58c6e94e38bb047ceec84a9b8e10d8e3
Nikolay Pultsin <geometer@fbreader.org>
2021-08-04 02:32:57 +0100

    removed duplicate check

--- qt/diagramWindow/diagramWindow_simplify.cpp
+++ qt/diagramWindow/diagramWindow_simplify.cpp
@@ -1,11 +1,8 @@
 #include "diagramWindow.h"
 
 void diagramWindow::simplify() {
-  if (!this->diagram.isClosed())
-    return;
-
-  if (this->diagram.simplify(2)) {
-    isSaved = false;
-    centralWidget()->repaint();
-  }
+	if (this->diagram.simplify(2)) {
+		isSaved = false;
+		centralWidget()->repaint();
+	}
 }

3846fa7480ef0732d309ed8cd7de819d9509715a
Nikolay Pultsin <geometer@fbreader.org>
2021-08-09 20:01:20 +0100

    code re-arranging

--- math/knot/knot_math.cpp
+++ math/knot/knot_math.cpp
@@ -4,6 +4,17 @@
 
 namespace KE { namespace ThreeD {
 
+const std::vector<double> KE::ThreeD::Knot::len_table() const {
+	const auto points = this->points();
+	std::vector<double> table;
+	table.reserve(points.size());
+	for (std::size_t i = 0; i < points.size() - 1; ++i) {
+		table.push_back(points[i].distanceTo(points[i + 1]));
+	}
+	table.push_back(points[points.size() - 1].distanceTo(points[0]));
+	return table;
+}
+
 double Knot::prmLength::compute() {
 	double value = 0.0;
 	for (const auto len : this->knot.len_table()) {

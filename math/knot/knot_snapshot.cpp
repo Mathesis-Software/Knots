a1dd03d55b5787ce8ab8c17ea35792ad343bd93f
Nikolay Pultsin <geometer@fbreader.org>
2021-08-10 02:36:14 +0100

    file renaming

--- math/knot/knot_snapshot.cpp
+++ math/knot/knot_snapshot.cpp
@@ -8,15 +8,15 @@ std::mutex snapshotMutex;
 
 }
 
-Knot::Snapshot::Snapshot(const Knot &knot, const std::vector<Point> &points) : knot(knot), points(new std::vector<Point>(points)), _edgeLengths(new std::vector<double>), generation(knot.generation()) {
-}
-
 Knot::Snapshot Knot::points() const {
 	if (this->lockCount > 0 || !this->latest || this->latest->generation != this->generation()) {
 		std::lock_guard<std::recursive_mutex> guard(this->dataChangeMutex);
 		this->latest = std::shared_ptr<Snapshot>(new Snapshot(*this, this->_points));
 	}
 	return *this->latest;
+}
+
+Knot::Snapshot::Snapshot(const Knot &knot, const std::vector<Point> &points) : knot(knot), points(new std::vector<Point>(points)), _edgeLengths(new std::vector<double>), generation(knot.generation()) {
 }
 
 const std::vector<double> &Knot::Snapshot::edgeLengths() const {

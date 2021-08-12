f215be2f79d9bb0b8bd4416ac7bf00102d3afd36
Nikolay Pultsin <geometer@fbreader.org>
2021-08-12 23:31:24 +0100

    rotation with pressed Alt/Shift

--- src/qt/gl/GLWidget_rotate.cpp
+++ src/qt/gl/GLWidget_rotate.cpp
@@ -1,12 +1,29 @@
 #include "GLWidget.h"
 
+#include <QtWidgets/QApplication>
+
 const double STEP = 0.006;
 
 #define		M(x,y)		this->currentMatrix[4*x+y]
 
 void GLWidget::rotate(const QPoint &start, const QPoint &end) {
-	const int dx = end.x() - start.x();
-	const int dy = end.y() - start.y();
+	int dx = end.x() - start.x();
+	int dy = end.y() - start.y();
+	int dz = 0;
+	const auto modifiers = QApplication::queryKeyboardModifiers();
+
+	if (modifiers & Qt::AltModifier) {
+		dz = dx;
+		dx = 0;
+		dy = 0;
+	} else if (modifiers & Qt::ShiftModifier) {
+		if (abs(dx) > abs(dy)) {
+			dy = 0;
+		} else {
+			dx = 0;
+		}
+	}
+
 
   double tmp;
 
@@ -20,6 +37,7 @@ void GLWidget::rotate(const QPoint &start, const QPoint &end) {
   tmp = M(0, 1); M(0, 1) = M(1, 0); M(1, 0) = tmp;
   tmp = M(0, 2); M(0, 2) = M(2, 0); M(2, 0) = tmp;
   tmp = M(2, 1); M(2, 1) = M(1, 2); M(1, 2) = tmp;
+
 	if (dx != 0) {
 		for (int i = 0; i < 3; ++i) {
 			tmp = M(i, 2) * sin(dx * STEP) + M(i, 0) * cos(dx * STEP);
@@ -32,6 +50,13 @@ void GLWidget::rotate(const QPoint &start, const QPoint &end) {
 			tmp = M(i, 1) * sin(dy * STEP) + M(i, 2) * cos(dy * STEP);
 			M(i, 1) = M(i, 1) * cos(dy * STEP) - M(i, 2) * sin(dy * STEP);
 			M(i, 2) = tmp;
+		}
+	}
+	if (dz != 0) {
+		for (int i = 0; i < 3; ++i) {
+			tmp = M(i, 0) * sin(dz * STEP) + M(i, 1) * cos(dz * STEP);
+			M(i, 0) = M(i, 0) * cos(dz * STEP) - M(i, 1) * sin(dz * STEP);
+			M(i, 1) = tmp;
 		}
 	}
 

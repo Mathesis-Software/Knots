cb6eadb28ee69888979c5bf8976619e3a40d1637
Nikolay Pultsin <geometer@fbreader.org>
2021-08-22 11:47:05 +0100

    Seifert surface: same color for front and back by default

--- src/qt/KnotOptionsDialog.cpp
+++ src/qt/KnotOptionsDialog.cpp
@@ -46,11 +46,13 @@ private:
 	const int _delta = 10;
 
 	const std::function<void(const Color&)> setter;
+
+public:
 	QColor currentColor;
 
 public:
 	ColorButton(const QString &title, const Color &initialColor, std::function<void(const Color&)> setter) : setter(setter) {
-		this->setColor(QColor(initialColor.red(), initialColor.green(), initialColor.blue()), false);
+		this->setColor(QColor(initialColor.red(), initialColor.green(), initialColor.blue()));
 		this->setSizePolicy(QSizePolicy(QSizePolicy::Maximum, QSizePolicy::Maximum));
 		this->setMinimumSize(_width + _delta, _height + _delta);
 		this->setIconSize(QSize(_width, _height));
@@ -69,8 +71,7 @@ public:
 		});
 	}
 
-private:
-	void setColor(const QColor &color, bool runSetter) {
+	void setColor(const QColor &color, bool runSetter = false) {
 		if (color == this->currentColor) {
 			return;
 		}
@@ -92,26 +93,27 @@ private:
 	}
 };
 
-struct ColorButtonWithLabel {
-	ColorButton *button;
+template<class W> struct WidgetWithLabel {
+	W *widget;
 	QLabel *label;
 
-	ColorButtonWithLabel(ColorButton *button, QLabel *label) : button(button), label(label) {
+	WidgetWithLabel(W *widget, QLabel *label) : widget(widget), label(label) {
 	}
 
 	void setEnabled(bool enabled) const {
-		button->setEnabled(enabled);
-		label->setEnabled(enabled);
+		this->widget->setEnabled(enabled);
+		this->label->setEnabled(enabled);
+	}
+
+	void addToLayout(QGridLayout *layout) {
+		const int row = layout->rowCount();
+		layout->addWidget(this->label, row, 0);
+		layout->addWidget(this->widget, row, 1);
 	}
 };
 
-ColorButtonWithLabel addColorButton(QGridLayout *layout, const QString &title, const Color &initialColor, std::function<void(const Color&)> setter) {
-	const int row = layout->rowCount();
-	auto label = new QLabel(title);
-	layout->addWidget(label, row, 0);
-	auto button = new ColorButton(title, initialColor, setter);
-	layout->addWidget(button, row, 1);
-	return ColorButtonWithLabel(button, label);
+WidgetWithLabel<ColorButton> addColorButton(const QString &title, const Color &initialColor, std::function<void(const Color&)> setter) {
+	return WidgetWithLabel(new ColorButton(title, initialColor, setter), new QLabel(title));
 }
 
 }
@@ -185,36 +187,61 @@ KnotOptionsDialog::KnotOptionsDialog(KnotWindow &window) {
 	});
 	layout->addWidget(thickness, 0, 1);
 
-	addColorButton(layout, "Background color", initialBackgroundColor, [widget](const Color &color) {
+	addColorButton("Background color", initialBackgroundColor, [widget](const Color &color) {
 		widget->_knot.setBackgroundColor(color);
 		widget->update();
-	});
-	addColorButton(layout, "Knot color", initialKnotColor, [widget](const Color &color) {
+	}).addToLayout(layout);
+	addColorButton("Knot color", initialKnotColor, [widget](const Color &color) {
 		widget->_knot.setKnotColor(color);
 		widget->update();
-	});
+	}).addToLayout(layout);
 
 	const int row = layout->rowCount();
 	layout->addWidget(new QLabel("Show Seifert surface"), row, 0);
 	auto seifertVisibility = new QCheckBox(QString());
 	layout->addWidget(seifertVisibility, row, 1);
-	auto seifertFront = addColorButton(layout, "Seifert surface color", initialSeifertFrontColor, [widget](const Color &color) {
-		widget->_knot.setSeifertFrontColor(color);
-		widget->update();
-	});
-	auto seifertBack = addColorButton(layout, "Seifert surface back color", initialSeifertBackColor, [widget](const Color &color) {
+
+	WidgetWithLabel<QCheckBox> sameColor(
+		new QCheckBox(QString()), new QLabel("Same color for front & back")
+	);
+	sameColor.widget->setChecked(initialSeifertFrontColor == initialSeifertBackColor);
+
+	auto seifertBack = addColorButton("Seifert surface back color", initialSeifertBackColor, [widget](const Color &color) {
 		widget->_knot.setSeifertBackColor(color);
 		widget->update();
 	});
-	QObject::connect(seifertVisibility, &QCheckBox::stateChanged, [widget, seifertFront, seifertBack](int state) {
+
+	auto seifertFront = addColorButton("Seifert surface color", initialSeifertFrontColor, [=](const Color &color) {
+		widget->_knot.setSeifertFrontColor(color);
+		if (sameColor.widget->isChecked()) {
+			widget->_knot.setSeifertBackColor(color);
+			seifertBack.widget->setColor(QColor(color.red(), color.green(), color.blue()));
+		}
+		widget->update();
+	});
+
+	seifertFront.addToLayout(layout);
+	sameColor.addToLayout(layout);
+	seifertBack.addToLayout(layout);
+
+	QObject::connect(seifertVisibility, &QCheckBox::stateChanged, [=](int state) {
 		const bool visible = state == ::Qt::Checked;
 		widget->setSeifertSurfaceVisibility(visible);
 		seifertFront.setEnabled(visible);
-		seifertBack.setEnabled(visible);
+		sameColor.setEnabled(visible);
+		seifertBack.setEnabled(visible && !sameColor.widget->isChecked());
+	});
+	QObject::connect(sameColor.widget, &QCheckBox::stateChanged, [=](int state) {
+		const bool same = state == ::Qt::Checked;
+		seifertBack.setEnabled(!same && seifertVisibility->isChecked());
+		if (same) {
+			seifertBack.widget->setColor(seifertFront.widget->currentColor, true);
+		}
 	});
 	seifertVisibility->setChecked(initialSeifertVisibility);
 	seifertFront.setEnabled(initialSeifertVisibility);
-	seifertBack.setEnabled(initialSeifertVisibility);
+	sameColor.setEnabled(initialSeifertVisibility);
+	seifertBack.setEnabled(initialSeifertVisibility && !sameColor.widget->isChecked());
 
 	for (int row = 0; row < layout->rowCount(); row += 1) {
 		layout->setRowMinimumHeight(row, 30);

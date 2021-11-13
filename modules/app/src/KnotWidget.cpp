e330dfd55bf85ce658fd08752af5a7db2a152bf7
Ilia Kriukov <ikrukov@gmail.com>
2021-11-13 18:37:49 +0300

    Format fix

--- modules/app/src/KnotWidget.cpp
+++ modules/app/src/KnotWidget.cpp
@@ -70,7 +70,7 @@ bool KnotWidget::isSmoothingInProgress() const {
 void KnotWidget::prepareMatrix(double *matrix) const {
 	for (int i = 0; i < 3; ++i) {
 		for (int j = 0; j < 3; ++j) {
-				matrix[4 * i + j] = this->knot.transformationMatrixElement(i, j);
+			matrix[4 * i + j] = this->knot.transformationMatrixElement(i, j);
 		}
 	}
 }
